36dd66524bdb6476659ca22ab562089896a7d7b9
Ryan Hellyer <ryanhellyer@gmail.com>
2022-11-16 21:12:32 +0100

    too lazy

--- words.php
+++ words.php
@@ -4,7 +4,10 @@ $get = filter_input( INPUT_GET, 'pages', FILTER_SANITIZE_SPECIAL_CHARS );
 $page_numbers = explode( ',', $get );
 $pages = array();
 foreach ( $page_numbers as $key => $page_number ) {
-	$pages[ $page_number ] = file_get_contents( 'page-' . $page_number . '.html' );
+	$path = 'page-' . $page_number . '.html';
+	if ( file_exists( $path ) ) {
+		$pages[ $page_number ] = file_get_contents( $path );
+	}
 }
 $shortened_json = json_encode( $pages, JSON_PRETTY_PRINT );
 
